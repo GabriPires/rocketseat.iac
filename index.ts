9b5fa7727d93db4fa3be5a4a58274410b5fe50a1
Gabriel Pires <gabpiresdev@gmail.com>
2025-05-05 11:27:32 -0300

    Add second S3 bucket and update references for improved resource management

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import * as aws from "@pulumi/aws";
 
-const bucket = new aws.s3.BucketV2("primeiro-bucket", {
+const firstBucket = new aws.s3.BucketV2("primeiro-bucket", {
+  bucket: "primeiro-bucket",
+  tags:{
+    IAC: 'true'
+  }
+});
+
+const secondBucket = new aws.s3.BucketV2("segundo-bucket", {
   bucket: "primeiro-bucket",
   tags:{
     IAC: 'true'
@@ -15,9 +22,13 @@ const ecr = new aws.ecr.Repository("primeiro-ecr", {
   }
 })
 
-export const bucketName = bucket.id;
-export const bucketRegion = bucket.region;
-export const bucketArn = bucket.arn;
+export const bucketName = firstBucket.id;
+export const bucketRegion = firstBucket.region;
+export const bucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketRegion = secondBucket.region;
+export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
